5ba8496b269e60882ebc6ca3650292c1f28dfaeb
Ben Hong <ben@bencodezen.io>
2021-07-21 18:59:22 -0400

    feature: add nuxt app with basic earthquake data

---
+++ pages/earthquakes/special.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+	<h1>Special Earthquake</h1>
+</template>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Hello VueDC July 2021!</h1>
+    <h2>Earthquake</h2>
+    <ul>
+      <li v-for="earthquake in earthquakeData.features" :key="earthquake.id">
+        {{ earthquake.id }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    earthquakeData: {}
+  }),
+  async created() {
+    const apiUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson'
+
+    const response = await fetch(apiUrl).then(data => data.json())
+
+    this.earthquakeData = response
+  }
+}
+</script>
