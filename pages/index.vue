ae2e923d58fc52e92526f2819402965ba85dfd62
Ben Hong <ben@bencodezen.io>
2021-07-21 19:46:43 -0400

    feature: add composition api with earthquake data

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,55 @@
 <template>
   <div>
     <h1>Hello VueDC July 2021!</h1>
+    <p>Count: {{ dataStore.count }}</p>
+    <button @click="increaseCount">Increment</button>
     <h2>Earthquake</h2>
     <ul>
-      <li v-for="earthquake in earthquakeData.features" :key="earthquake.id">
-        {{ earthquake.id }}
+      <li v-for="(box, index) in earthquakeData.bbox" :key="`bbox-${index}`">
+        {{ box }}
       </li>
     </ul>
+    <p>{{ coordinateInput }}</p>
+    <input type="number" v-model="coordinateInput" />
+    <button @click="addNewCoordinate">Add new coordinate</button>
   </div>
 </template>
 
 <script>
+import { reactive, ref, useAsync } from '@nuxtjs/composition-api'
+
 export default {
-  data: () => ({
-    earthquakeData: {}
-  }),
-  async created() {
-    const apiUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson'
+  setup() {
+    const dataStore = reactive({
+      count: 10
+    })
 
-    const response = await fetch(apiUrl).then(data => data.json())
+    const increaseCount = () => {
+      dataStore.count++
+    }
 
-    this.earthquakeData = response
+    const earthquakeData = useAsync(async () => {
+      const apiUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson'
+
+      const response = await fetch(apiUrl).then(data => data.json())
+
+      return response
+    })
+
+    const coordinateInput = ref(46)
+
+    const addNewCoordinate = () => {
+      console.log('add new coordinate')
+      earthquakeData.value.bbox.push(coordinateInput.value)
+    }
+
+    return {
+      addNewCoordinate,
+      coordinateInput,
+      dataStore,
+      earthquakeData,
+      increaseCount
+    }
   }
 }
 </script>
